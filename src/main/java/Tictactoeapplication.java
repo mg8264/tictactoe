12ecfad2058e0d2d2c6833980490e708197697d2
mg8264 <mg8264@srmist.edu.in>
2026-05-06 13:34:39 +0530

    UC7

--- src/main/java/Tictactoeapplication.java
+++ src/main/java/Tictactoeapplication.java
@@ -1,26 +1,38 @@
-import java.util.Scanner;
+import java.util.Random;
 
-public class UC6 {
+public class UC7 {
+
+    static char[][] board = {
+            {'-', '-', '-'},
+            {'-', '-', '-'},
+            {'-', '-', '-'}
+    };
+
+    static char computerSymbol = 'O';
+
     public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-        int row=sc.nextInt();
-        int col=sc.nextInt();
-        char symbol=sc.next().charAt(0);
-
-        placeMove(row,col,symbol);
-
+        computerMove();
+        printBoard();
     }
-    public static void placeMove(int row,int col,char symbol){
 
-        for(int i=0;i<3;i++){
-            for(int j=0;j<3;j++){
-                if(i==row && j==col){
-                    System.out.print(symbol);
-                }
-                else {
-                    System.out.print("_");
-                }
+    static void computerMove() {
+        Random rand = new Random();
+
+        while (true) {
+            int row = rand.nextInt(3);
+            int col = rand.nextInt(3);
+
+            if (board[row][col] == '-') {
+                board[row][col] = computerSymbol;
+                break;
+            }
+        }
+    }
+
+    static void printBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " ");
             }
             System.out.println();
         }
