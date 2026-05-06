4e0d1e8b56f24c92d82b8bc66cfe78e8a7e93c91
mg8264 <mg8264@srmist.edu.in>
2026-05-06 13:13:22 +0530

    UC2

--- src/main/java/Tictactoeapplication.java
+++ src/main/java/Tictactoeapplication.java
@@ -1,10 +1,18 @@
-public class UC1 {
+import java.util.Random;
+
+public class UC2 {
 
     static char[][] board = new char[3][3];
 
+    static char player1Symbol;
+    static char player2Symbol;
+    static char currentPlayerSymbol;
+
     public static void main(String[] args) {
         initializeBoard();
+        performToss();
         printBoard();
+
     }
 
     public static void initializeBoard() {
@@ -12,6 +20,28 @@ public class UC1 {
             for (int j = 0; j < 3; j++) {
                 board[i][j] = '_';
             }
+        }
+    }
+
+
+    public static void performToss() {
+        Random rand = new Random();
+        int toss = rand.nextInt(2);
+
+        if (toss == 0) {
+            player1Symbol = 'X';
+            player2Symbol = 'O';
+            currentPlayerSymbol = player1Symbol;
+
+            System.out.println("Player 1 starts and is 'X'");
+            System.out.println("Player 2 is 'O'");
+        } else {
+            player1Symbol = 'O';
+            player2Symbol = 'X';
+            currentPlayerSymbol = player2Symbol;
+
+            System.out.println("Player 2 starts and is 'X'");
+            System.out.println("Player 1 is 'O'");
         }
     }
 
