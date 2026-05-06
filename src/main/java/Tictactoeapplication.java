d81ec96bb04cdbacaa8a7a9520db2f5112cc3607
mg8264 <mg8264@srmist.edu.in>
2026-05-06 13:45:20 +0530

    UC10

--- src/main/java/Tictactoeapplication.java
+++ src/main/java/Tictactoeapplication.java
@@ -1,4 +1,4 @@
-public class UC9 {
+public class UC10 {
 
     static char[][] board = new char[3][3];
 
@@ -7,45 +7,20 @@ public class UC9 {
         board = new char[][] {
                 {'X', 'O', 'X'},
                 {'O', 'X', 'O'},
-                {'O', 'X', 'X'}
+                {'O', 'X', 'O'}
         };
 
-        System.out.println(hasWon('X')); // true
+        System.out.println(isDraw());
     }
 
-
-    static boolean hasWon(char symbol) {
-
-        for (int i = 0; i < 3; i++) {
-            if (board[i][0] == symbol &&
-                    board[i][1] == symbol &&
-                    board[i][2] == symbol) {
-                return true;
+    static boolean isDraw() {
+        for (int r = 0; r < 3; r++) {
+            for (int c = 0; c < 3; c++) {
+                if (board[r][c] == '-') {
+                    return false;
+                }
             }
         }
-
-        for (int j = 0; j < 3; j++) {
-            if (board[0][j] == symbol &&
-                    board[1][j] == symbol &&
-                    board[2][j] == symbol) {
-                return true;
-            }
-        }
-
-
-        if (board[0][0] == symbol &&
-                board[1][1] == symbol &&
-                board[2][2] == symbol) {
-            return true;
-        }
-
-
-        if (board[0][2] == symbol &&
-                board[1][1] == symbol &&
-                board[2][0] == symbol) {
-            return true;
-        }
-
-        return false;
+        return true;
     }
 }
